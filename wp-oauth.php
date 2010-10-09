73026345c56808b46fe6f4860b182c9d022ffb99
louyx <louyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-09 13:53:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tp/tags/1.0@298350 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-oauth.php
+++ wp-oauth.php
@@ -8,25 +8,17 @@ if( !defined( 'WP_OAUTH' ) ) :
 
 define('WP_OAUTH', true);
 
-global $oauth_activate;
-if( !isset($oauth_activate) )
-	$oauth_activate = false;
-
 /**
- * Don't forget to set $oauth_activate to true when you activate your plugin.
+ * Don't forget to call oauth_activate() when you activate your plugin.
  *  just to make sure rewrite rules will be flushed.
  */
 
 add_action('init', 'oauth_init');
 function oauth_init() {
-	global $oauth_activate, $wp;
+	global $wp, $oauth_activate;
 	
 	add_rewrite_rule('oauth/(.+?)/?', 'index.php?oauth=$matches[1]',1);
 	add_rewrite_rule('oauth/?', 'index.php?oauth=null',1);
-	
-	if( $oauth_activate ) {
-		flush_rewrite_rules();
-	}
 	
 	$wp->add_query_var('oauth');
 	
@@ -36,11 +28,14 @@ add_action('template_redirect', 'oauth_template_redirect');
 function oauth_template_redirect() {
 	if( get_query_var('oauth') ) {
 		$oauth_sites = apply_filters('oauth_sites', array());
-		if( !in_array(get_query_var('oauth'), $oauth_sites)) {
+		$site = get_query_var('oauth');
+		if( substr($site, -1, 1) == '/' )
+			$site = substr($site, 0, -1);
+		if( !in_array($site, $oauth_sites)) {
 			do_action('wp_oauth_unknown_site');
 			die( __('OAuth site not recognized!') );
 		}
-		do_action('oauth_start_'.get_query_var('oauth'));
+		do_action('oauth_start_'.$site);
 		die();
 	}
 }
@@ -48,11 +43,12 @@ function oauth_template_redirect() {
 function oauth_link($site,$args=array()){
 	$link = get_bloginfo('url');
 	$link .= '/oauth/' . $site;
-	foreach( $args as $k => $v ) {
-		$link = add_query_arg($link, $k, $v);
-	}
+	$link = add_query_arg($args, $link);
 	return $link;
 }
 
-endif;
+function oauth_activate() {
+	delete_option('rewrite_rules');
+}
 
+endif;
